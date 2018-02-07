c8e7577602ec6046edf68826e18fff4196f82988
Yuriy Shandor <yuriyshandor@gmail.com>
2018-02-07 10:05:33 +0200

    Add Section Main Content. Mod_01

--- js/script.js
+++ js/script.js
@@ -14,4 +14,45 @@ $(document).ready(function() {
     $(this).toggleClass('toggler-icon_close');
     $('.nav-bar').slideToggle('0.5s');
   });
+
+  //Price clider
+  $("#price").slider({
+	  min: 0,
+	  max: 250,
+	  values: [0,250],
+	  range: true,
+    stop: function(event, ui) {
+		  jQuery("input#minCost").val(jQuery("#price").slider("values",0));
+		  jQuery("input#maxCost").val(jQuery("#price").slider("values",1));
+    },
+    slide: function(event, ui){
+		  jQuery("input#minCost").val(jQuery("#price").slider("values",0));
+		  jQuery("input#maxCost").val(jQuery("#price").slider("values",1));
+    }
+  });
+
+  $("input#minCost").change(function(){
+	  var value1=$("input#minCost").val();
+	  var value2=$("input#maxCost").val();
+
+    if(parseInt(value1) > parseInt(value2)){
+		  value1 = value2;
+		  $("input#minCost").val(value1);
+	  }
+	  $("#price").slider("values",0,value1);
+  });
+
+
+  $("input#maxCost").change(function(){
+	  var value1=$("input#minCost").val();
+	  var value2=$("input#maxCost").val();
+
+	  if (value2 > 250) { value2 = 250; jQuery("input#maxCost").val(250)}
+
+	  if(parseInt(value1) > parseInt(value2)){
+		  value2 = value1;
+		  $("input#maxCost").val(value2);
+	  }
+	  $("#price").slider("values",1,value2);
+  });
 });
