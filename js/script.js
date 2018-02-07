851b633337be7c97d76a1bdac7a87975d98aa5de
Yuriy Shandor <yuriyshandor@gmail.com>
2018-02-07 13:48:48 +0200

    Add Section Main Content. Mod_03

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+  $('select').niceSelect();
+  
   // Change header on scroll
   $(window).scroll(function(){
     if ($(window).scrollTop()) {
@@ -15,11 +17,11 @@ $(document).ready(function() {
     $('.nav-bar').slideToggle('0.5s');
   });
 
-  //Price clider
+  //Price clider by jQuery Slider UI
   $("#price").slider({
-	  min: 0,
-	  max: 250,
-	  values: [0,250],
+	  min: 70,
+	  max: 800,
+	  values: [70,800],
 	  range: true,
     stop: function(event, ui) {
 		  jQuery("input#minCost").val(jQuery("#price").slider("values",0));
@@ -47,7 +49,7 @@ $(document).ready(function() {
 	  var value1=$("input#minCost").val();
 	  var value2=$("input#maxCost").val();
 
-	  if (value2 > 250) { value2 = 250; jQuery("input#maxCost").val(250)}
+	  if (value2 > 800) { value2 = 800; jQuery("input#maxCost").val(250)}
 
 	  if(parseInt(value1) > parseInt(value2)){
 		  value2 = value1;
@@ -55,4 +57,19 @@ $(document).ready(function() {
 	  }
 	  $("#price").slider("values",1,value2);
   });
+
+  //Categories accordion
+  var accordionsArr = $(".accordion");
+
+  for (i = 0; i < accordionsArr.length; i++) {
+    accordionsArr[i].addEventListener("click", function() {
+      this.classList.toggle("accordion_active");
+      var panel = this.nextElementSibling;
+      if (panel.style.height){
+        panel.style.height = null;
+      } else {
+        panel.style.height = panel.scrollHeight + "px";
+      }
+    });
+  }
 });
